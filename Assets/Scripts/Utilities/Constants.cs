ca05e0ec246616011f68534f77abb1f6f2d99459
GRANDY-4 <23rmenton@firhouseetss.ie>
2026-06-12 23:37:07 +0100

    Add Constants utility class

---
+++ Assets/Scripts/Utilities/Constants.cs
@@ -0,0 +1,21 @@
+namespace MIR.Utilities
+{
+    /// <summary>
+    /// Global constants used throughout the game.
+    /// </summary>
+    public static class Constants
+    {
+        public const string GAME_NAME = "Make It Rain";
+        public const string GAME_VERSION = "0.0.1";
+
+        // Tags
+        public const string TAG_PLAYER = "Player";
+        public const string TAG_ENEMY = "Enemy";
+        public const string TAG_LOOT = "Loot";
+
+        // Layers
+        public const int LAYER_PLAYER = 8;
+        public const int LAYER_ENEMY = 9;
+        public const int LAYER_GROUND = 10;
+    }
+}
